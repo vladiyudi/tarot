f958847c93436fa0617c234bf763b8d61fb02a5b
vladiyudi <plastikwood@gmail.com>
2023-08-18 18:30:31 +0300

    tarot

--- pages/api/predictions.js
+++ pages/api/predictions.js
@@ -14,7 +14,7 @@ const handler = async (req, res) => {
 
 
     // res.status(200).json({ images: imageUrls, prediction: predictionText.content })
-    res.status(200).json({ images: 'https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/0-70c9f6c2-2ceb-44f2-a8ab-a96f87ed5376.png', prediction: predictionText.content })
+    res.status(200).json({ images: imageUrls, prediction: 'predictionText.content' })
 
 }
 
