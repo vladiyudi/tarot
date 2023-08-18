cad36c76d6a6839477297441990ff38c81404a08
vladiyudi <plastikwood@gmail.com>
2023-08-18 18:49:30 +0300

    tarot

--- pages/api/predictions.js
+++ pages/api/predictions.js
@@ -15,7 +15,7 @@ const handler = async (req, res) => {
     const imageUrls = await GenerateImage('futuristic women')
 
 
-    res.status(200).json({ images: 'https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/0-1a1ca9c3-b06f-4c69-8af3-f55654838d13.png', prediction: 'predictionText.content' })
+    res.status(200).json({ images: imageUrls, prediction: 'predictionText.content' })
     // res.status(200).json({ images: imageUrls, prediction: 'predictionText.content' })
 
 }
