dd69708314a5d215028877184735a8b8675e4e49
vladiyudi <plastikwood@gmail.com>
2023-08-18 21:20:39 +0300

    tarot

--- utils/GetOpenAiPrediction.js
+++ utils/GetOpenAiPrediction.js
@@ -5,42 +5,27 @@ const client = axios.create({
 });
 
 
-
-export default async function fetchOpenAI (prompt) {
-
-    const params = {
-      "prompt": prompt, 
-      "max_tokens": 50
-    }
-    try {
-        const res = await client.post('https://api.openai.com/v1/engines/davinci/completions', params)
-        
-        console.log(res.data)
-        
-        return res.data.choices[0].text
-    }
-    catch (error) {
-        console.log(error)
-    }
-
-}
-
   
-//  export default async function fetchOpenAI (prompt) {
-//     const response = await fetch('https://api.openai.com/v1/chat/completions', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-//         },
-//         body: JSON.stringify({
-//             model: 'gpt-3.5-turbo',
-//             messages: [{content: prompt, role: 'user' }],
-//             temperature: 0.9,
-//             top_p: 1,
-//         }),
-//     });
-//     const data = await response.json();
-//     return data.choices[0].message.content
-//  } 
+ export default async function fetchOpenAI (prompt) {
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages: [{content: prompt, role: 'user' }],
+                temperature: 0.9,
+                top_p: 1,
+            }),
+        });
+        const data = await response.json();
+        return data.choices[0].message.content
+    } catch (error) {
+        console.log(error)
+        fetchOpenAI(prompt)
+    }
+ } 
 
