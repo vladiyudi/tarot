f7d32bb59d52ad1581798bc9d6fb7f8ad33af785
vladiyudi <plastikwood@gmail.com>
2023-08-18 20:57:20 +0300

    tarot

--- utils/GetOpenAiPrediction.js
+++ utils/GetOpenAiPrediction.js
@@ -6,41 +6,41 @@ const client = axios.create({
 
 
 
-// export default async function fetchOpenAI (prompt) {
+export default async function fetchOpenAI (prompt) {
 
-//     const params = {
-//       "prompt": prompt, 
-//       "max_tokens": 50
-//     }
-//     try {
-//         const res = await client.post('https://api.openai.com/v1/engines/davinci/completions', params)
+    const params = {
+      "prompt": prompt, 
+      "max_tokens": 50
+    }
+    try {
+        const res = await client.post('https://api.openai.com/v1/engines/davinci/completions', params)
         
-//         console.log(res.data)
+        console.log(res.data)
         
-//         return res.data.choices[0].text
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
+        return res.data.choices[0].text
+    }
+    catch (error) {
+        console.log(error)
+    }
 
-// }
+}
 
   
- export default async function fetchOpenAI (prompt) {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: [{content: prompt, role: 'user' }],
-            temperature: 0.9,
-            top_p: 1,
-        }),
-    });
-    const data = await response.json();
-    return data.choices[0].message.content
- } 
+//  export default async function fetchOpenAI (prompt) {
+//     const response = await fetch('https://api.openai.com/v1/chat/completions', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+//         },
+//         body: JSON.stringify({
+//             model: 'gpt-3.5-turbo',
+//             messages: [{content: prompt, role: 'user' }],
+//             temperature: 0.9,
+//             top_p: 1,
+//         }),
+//     });
+//     const data = await response.json();
+//     return data.choices[0].message.content
+//  } 
 
