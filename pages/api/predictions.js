7e8b429b62d2b662a82799fc634b1281976b335f
vladiyudi <plastikwood@gmail.com>
2023-08-18 18:42:06 +0300

    tarot

--- pages/api/predictions.js
+++ pages/api/predictions.js
@@ -4,15 +4,15 @@ import GenerateImage from '../../utils/GetStabeleDiffusionImage'
 
 const handler = async (req, res) => {
 
-    const predictionText = await getPredictionText(req.body.data)
+    // const predictionText = await getPredictionText(req.body.data)
     
-    const prompt = await createPrompt(predictionText.content)
+    // const prompt = await createPrompt(predictionText.content)
     
-    console.log("PROMPT", prompt.content)
+    // console.log("PROMPT", prompt.content)
+
+    // // const imageUrls = await GenerateImage(prompt.content)
 
     // const imageUrls = await GenerateImage(prompt.content)
-
-    const imageUrls = await GenerateImage(prompt.content)
 
 
     res.status(200).json({ images: 'https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/0-1a1ca9c3-b06f-4c69-8af3-f55654838d13.png', prediction: 'predictionText.content' })
